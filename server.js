cf164131575670c88f179f0854a1b33ec8dd4b98
sheheer <muhammedsheheer99@gmail.com>
2024-09-20 02:47:21 +0530

    orgin added correct

--- server.js
+++ server.js
@@ -45,6 +45,10 @@ import cookieParser from "cookie-parser";
 import connectDb from "./config/mongodb.js";
 import morgan from "morgan";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 connectDb();
@@ -67,16 +71,13 @@ app.use(morgan("dev"));
 app.use(express.json({ limit: "150mb" }));
 app.use(express.urlencoded({ limit: "150mb", extended: true }));
 app.use(cookieParser());
+
 app.use("/api", appRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
-app.get("/", (req, res) => {
-	res.json({ message: "Server is running" });
 });
 
 app.listen(PORT, () => {
