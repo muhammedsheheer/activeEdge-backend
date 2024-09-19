c4fecbf4ddc1865b88da48da58f65ca631b26fd3
sheheer <muhammedsheheer99@gmail.com>
2024-09-20 00:51:04 +0530

    google auth updated

--- config/googleConfig.js
+++ config/googleConfig.js
@@ -3,8 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const redirectUri =
+	process.env.NODE_ENV === "production"
+		? "https://active-edge-client.onrender.com"
+		: "http://localhost:3000";
+
 export const client = new OAuth2Client(
 	process.env.CLIENT_ID,
 	process.env.CLIENT_SECRET,
-	"http://localhost:3000"
+	redirectUri
 );
