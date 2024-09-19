4f5e961d4e4179943606d04824b032331e81300b
sheheer <muhammedsheheer99@gmail.com>
2024-09-19 23:42:29 +0530

    origin updated

--- server.js
+++ server.js
@@ -1,4 +1,43 @@
-import express, { urlencoded } from "express";
+// import express, { urlencoded } from "express";
+// import cors from "cors";
+// import "dotenv/config";
+// import appRoutes from "./routes/apiRoutes.js";
+// import cookieParser from "cookie-parser";
+// import connectDb from "./config/mongodb.js";
+// import morgan from "morgan";
+
+// const PORT = process.env.PORT || 5000;
+// connectDb();
+
+// const app = express();
+
+// const corsOrigin =
+// 	process.env.NODE_ENV === "production"
+// 		? "https://active-front-end.vercel.app"
+// 		: "http://localhost:3000";
+
+// app.use(
+// 	cors({
+// 		origin: "*",
+// 		credentials: true,
+// 	})
+// );
+
+// app.use(morgan("dev"));
+// app.use(express.json({ limit: "150mb" }));
+// app.use(express.urlencoded({ limit: "150mb", extended: true }));
+// app.use(cookieParser());
+// app.use("/api", appRoutes);
+
+// app.get("/", (req, res) => {
+// 	res.json({ message: "Servor is running" });
+// });
+
+// app.listen(PORT, () => {
+// 	console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import appRoutes from "./routes/apiRoutes.js";
@@ -18,7 +57,7 @@ const corsOrigin =
 
 app.use(
 	cors({
-		origin: "*",
+		origin: corsOrigin,
 		credentials: true,
 	})
 );
@@ -30,7 +69,7 @@ app.use(cookieParser());
 app.use("/api", appRoutes);
 
 app.get("/", (req, res) => {
-	res.json({ message: "Servor is running" });
+	res.json({ message: "Server is running" });
 });
 
 app.listen(PORT, () => {
