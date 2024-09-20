c1c61b7a8f4c299c1b20b963e7f22333a9d516d2
sheheer <muhammedsheheer99@gmail.com>
2024-09-20 13:08:34 +0530

    jwt updated push new one

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,20 +1,5 @@
 import jwt from "jsonwebtoken";
 
-// export const generateToken = (res, user) => {
-// 	const token = jwt.sign(
-// 		{ id: user._id, role: user.role, name: user.name },
-// 		process.env.SECRET_KEY,
-// 		{ expiresIn: "5h" }
-// 	);
-
-// 	res.cookie("jwtToken", token, {
-// 		httpOnly: true,
-// 		secure: process.env.NODE_ENV === "production",
-// 		maxAge: 5 * 60 * 60 * 1000,
-// 		sameSite: "None",
-// 	});
-// };
-
 export const generateToken = (res, user) => {
 	const token = jwt.sign(
 		{ id: user._id, role: user.role, name: user.name },
@@ -27,6 +12,5 @@ export const generateToken = (res, user) => {
 		secure: process.env.NODE_ENV === "production",
 		maxAge: 5 * 60 * 60 * 1000,
 		sameSite: "None",
-		domain: "active-front-end-mohammed-sheheer-cbs-projects.vercel.app",
 	});
 };
