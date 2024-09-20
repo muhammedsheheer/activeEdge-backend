ddbebc78cc6f3d981fe094dd1cd6ddf607d529ce
sheheer <muhammedsheheer99@gmail.com>
2024-09-20 12:16:37 +0530

    jwt updated new

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,4 +1,19 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
+
+// export const generateToken = (res, user) => {
+// 	const token = jwt.sign(
+// 		{ id: user._id, role: user.role, name: user.name },
+// 		process.env.SECRET_KEY,
+// 		{ expiresIn: "5h" }
+// 	);
+
+// 	res.cookie("jwtToken", token, {
+// 		httpOnly: true,
+// 		secure: process.env.NODE_ENV === "production",
+// 		maxAge: 5 * 60 * 60 * 1000,
+// 		sameSite: "None",
+// 	});
+// };
 
 export const generateToken = (res, user) => {
 	const token = jwt.sign(
@@ -7,16 +22,12 @@ export const generateToken = (res, user) => {
 		{ expiresIn: "5h" }
 	);
 
-	// res.cookie("jwtToken", token, {
-	// 	httpOnly: true,
-	// 	secure: process.env.NODE_ENV === "production",
-	// 	maxAge: 60 * 60 * 5000,
-	// });
-
 	res.cookie("jwtToken", token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
 		maxAge: 5 * 60 * 60 * 1000,
-		sameSite: "None",
+		sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+		domain:
+			"https://active-front-end-mohammed-sheheer-cbs-projects.vercel.app/",
 	});
 };
