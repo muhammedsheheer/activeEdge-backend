187777a301cba9a1c9e10f376c33dc0aa7bb4b69
sheheer <muhammedsheheer99@gmail.com>
2024-09-30 19:18:39 +0530

    front api changed now

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_URI);
+		await mongoose.connect(
+			process.env.MONGO_URI ||
+				"mongodb+srv://muhammedsheheer99:ahSTaVwgEJba499x@cluster0.r5h8i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+		);
 		console.log("Connected to MongoDB!");
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error);
